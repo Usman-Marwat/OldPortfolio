369da8f3eaf4001ddb7e880ba2431a72c04fafce
Usman-Marwat <us895907a2@gmail.com>
2023-04-02 22:44:46 +0500

    Build mobile nav componeent with JS

--- src/main.js
+++ src/main.js
@@ -7,4 +7,13 @@ import '../styles/components/featured.css';
 import '../styles/components/work.css';
 import '../styles/components/contact.css';
 import '../styles/components/footer.css';
+import '../styles/components/mobile-nav.css';
 import '../styles/utils.css';
+
+import mobileNav from './utils/mobile-nav';
+// import darkMode from './utils/dark-mode';
+// import lazyLoading from './utils/lazy-loading';
+
+mobileNav();
+// darkMode();
+// lazyLoading();
