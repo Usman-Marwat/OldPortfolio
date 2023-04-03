2be0e6077cc9058c79f836586891a280ca8b7e1d
Usman-Marwat <us895907a2@gmail.com>
2023-04-03 06:02:56 +0500

    Implement lazy loading and create custom scrollbar

--- src/main.js
+++ src/main.js
@@ -11,9 +11,9 @@ import '../styles/components/mobile-nav.css';
 import '../styles/utils.css';
 
 import mobileNav from './utils/mobile-nav';
-// import darkMode from './utils/dark-mode';
-// import lazyLoading from './utils/lazy-loading';
+import darkMode from './utils/dark-mode';
+import lazyLoading from './utils/lazy-loading';
 
 mobileNav();
-// darkMode();
-// lazyLoading();
+darkMode();
+lazyLoading();
